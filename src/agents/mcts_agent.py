a262cadd5ce0c0450dbf09e174bb9aa61a70c9eb
Edgar <andradeed78@gmail.com>
2026-01-27 14:21:42 -0800

    add requirements

--- src/agents/mcts_agent.py
+++ src/agents/mcts_agent.py
@@ -1,4 +1,5 @@
 from stable_baselines3 import A2C
+from typing import Any
 
 from agents.base_classes import GameProtocol
 from agents.random_policy import GameUniformPolicy
@@ -8,5 +9,3 @@ class StBlsA2C(GameUniformPolicy):
     def __init__(self, game: GameProtocol, rng: Any | None = None) -> None:
         super().__init__(game, rng)
         
-
-    def 
